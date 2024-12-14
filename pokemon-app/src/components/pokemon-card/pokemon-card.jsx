5e2d2fd262ef0717730b693d3d8b7175023f501b
Carla Lopez <iamcarla.28@gmail.com>
2024-12-14 11:53:43 +0100

    Pokemon card design

--- pokemon-app/src/components/pokemon-card/pokemon-card.jsx
+++ pokemon-app/src/components/pokemon-card/pokemon-card.jsx
@@ -1,12 +1,24 @@
+import "./pokemon-card.css"
 
 function PokemonCard ({pokemon}){
-
+    function formatId(id){
+        let formattedId;
+        if (id.toString().length === 1){
+            formattedId = `00${pokemon.id}`
+        }
+        else if (id.toString().length === 2){
+            formattedId = `0${pokemon.id}`}
+        else ( formattedId = `${pokemon.id}`)
+        return formattedId
+    }
+   
     return (
         <div className="pokemon-card">
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} className="card-img-top" alt={pokemon.name} />
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} className="card-img-top" alt={pokemon.name} style={{ width: '200px', height: '200px' }} />
             <div className="card-body">
-                <h4 className="card-title mb-1 fw-light text-break">{pokemon.name} </h4>
-                <h3 className= "text-lg font-semibold text-gray-800"> {pokemon.id} </h3>
+                <h3 className= "fs-6 text-secondary"> Nº {formatId(pokemon.id)} </h3>
+                <h4 className="card-title mb-1 text-break">{pokemon.name} </h4>
+                
             </div>
         </div>
     )
