376bd53d65345cbecce7b496080454074c36f167
Carla Lopez <iamcarla.28@gmail.com>
2025-01-08 17:37:35 +0100

    Added delete team

--- pokemon-app/src/components/bubble-card/bubble-card.jsx
+++ pokemon-app/src/components/bubble-card/bubble-card.jsx
@@ -1,17 +1,34 @@
 import { useNavigate } from "react-router-dom";
 import "./bubble-card.css";
+import { useState } from "react";
+import { useTeam } from "../../context/TeamContext";
 
 const BubbleCard = ({ pokemon }) => {
   const navigate = useNavigate();  
+  const {team, setTeam} = useTeam(); 
 
   const handleCardClick = () => {
     navigate(`/pokedex/${pokemon.id}`, { replace: true });
     window.location.reload();  
   };
 
+  function eliminateFavorite (item) {
+    setTeam ((prev)=> 
+    prev.filter((team)=> team !== item )
+    )
+  }
+
+  function handleEliminateClick (e) {
+    e.stopPropagation(); 
+    eliminateFavorite(pokemon.id)
+  }
+
   return (
     <div className="d-flex flex-wrap justify-content-center gap-4">
       <div className="bubbleCard">
+        <div className="remove" onClick={handleEliminateClick}>
+          <ion-icon name="remove-circle"  style={{ color:'lightcoral'}}></ion-icon>
+        </div>
         <div className="bubble-link" onClick={handleCardClick}>
           <img 
             src={pokemon.sprites.other["official-artwork"].front_default} 
--- pokemon-app/src/pages/favorites-page.jsx
+++ pokemon-app/src/pages/favorites-page.jsx
@@ -1,8 +1,18 @@
 import FavoritesList from "../components/favs-list/favs-list";
+import TeamList from "../components/team-list/team-list";
+import "../pages/favorites-page.css"
 
 const FavoritesPage = () => {
-    return <div>Favorites Page
-    <FavoritesList/> </div>;
+  return (
+    <div className="favorites-page">
+        <div className="team">
+            <TeamList />
+        </div>
+        <div className="favorites">
+            <FavoritesList />
+        </div>
+    </div>
+    );
   };
   
   export default FavoritesPage;
