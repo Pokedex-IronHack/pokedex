d6a8e699e748b5b15af6fd3dcd5e004c27283519
Nuria <tu.email@ejemplo.com>
2025-01-12 20:55:02 +0100

    centrar y cambio de estilo deck

--- pokemon-app/src/pages/deck-page.jsx
+++ pokemon-app/src/pages/deck-page.jsx
@@ -1,46 +1,26 @@
 import DeckCard from "../components/card-deck/card-deck";
 import { useTeam } from "../context/TeamContext";
-import "../pages/deck-page.css" 
-import Slider from "react-slick"; 
+import "../pages/deck-page.css";
 
 function Deck() {
-  const { team } = useTeam(); 
-
-  const settings = {
-    centerMode: true,
-    centerPadding: "0px",
-    slidesToShow: 3,
-    focusOnSelect: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          centerMode: true,
-        },
-      },
-    ],
-  };
+  const { team } = useTeam();
 
   return (
     <div className="deck">
-    <h1 className="title">Deck</h1>
-    {team.length > 0 ? (
-      <Slider {...settings} className="carousel">
-        {team.map((id) => (
-          <div key={id} className="carousel-card">
-            <DeckCard pokedexId={id} />
-          </div>
-        ))}
-      </Slider>
-    ) : (
-      <p>No Pokémon in your team yet!</p>
-    )}
-  </div>
-);
+      <h1 className="titol">Deck</h1>
+      {team.length > 0 ? (
+        <div className="deck-cards">
+          {team.map((id) => (
+            <div key={id} className="deck-card">
+              <DeckCard pokedexId={id} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No Pokémon in your team yet!</p>
+      )}
+    </div>
+  );
 }
 
 export default Deck;
